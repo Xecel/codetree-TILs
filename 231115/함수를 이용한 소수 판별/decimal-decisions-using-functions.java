43fe8168f13571a3d731e1509527b9d63e6ce362
Xecel <146149722+Xecel@users.noreply.github.com>
2023-11-15 19:18:11 +0900

    Title: 함수를 이용한 소수 판별, Time: 154ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 231115/함수를 이용한 소수 판별/decimal-decisions-using-functions.java
@@ -0,0 +1,41 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        int a = scanInt();
+        int b = scanInt();
+
+        eratos(a, b);
+    }
+
+    public static void eratos(int st, int ed) {
+        boolean[] isPrime = new boolean[ed + 1];
+
+        for (int i = 1; i <= ed; i++) {
+            isPrime[i] = true;
+        }
+
+        for (int i = 2; i * i <= ed; i++) {
+            if (isPrime[i]) {
+                for (int j = i * i; j <= ed; j += i) {
+                    isPrime[j] = false;
+                }
+            }
+        }
+
+        int sum = 0;
+        for (int v = st; v <= ed; v++) {
+            if (isPrime[v]) {
+                sum += v;
+            }
+        }
+
+        System.out.println(sum);
+    }
+
+
+    public static Scanner sc = new Scanner(System.in);
+    public static int scanInt() {
+        return sc.nextInt();
+    }
+}
