756d77b26b92d01ae21e76df84f08e06c8296e87
Xecel <146149722+Xecel@users.noreply.github.com>
2023-11-17 20:55:10 +0900

    Title: 두 정수에 대한 연산값 2, Time: 119ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 231117/두 정수에 대한 연산값 2/operational-values-for-two-integers-2.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        int a = scanInt();
+        int b = scanInt();
+
+        fn(a, b);
+    }
+
+    public static void fn(int a, int b) {
+        if (a < b) {
+            System.out.println((a + 10) + " " + (b * 2));
+        } else {
+            System.out.println((a * 10) + " " + (b + 10));
+        }
+    }
+
+    public static Scanner sc = new Scanner(System.in);
+    public static int scanInt() {
+        return sc.nextInt();
+    }
+
+}
