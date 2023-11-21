2c73eaccccf8a6d960c1927f1deb075ff8c4fe97
Xecel <146149722+Xecel@users.noreply.github.com>
2023-11-22 00:59:21 +0900

    Title: 재귀함수의 꽃, Time: 118ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 231122/재귀함수의 꽃/the-flower-of-the-recursive-function.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+public class Main {
+     public static void main(String[] args) {
+
+        int N = scanInt();
+        if (N == 1) {
+            System.out.println("1 1");
+        } else {
+            recurvePrint(N, N, true);
+        }
+    }
+
+    public static void recurvePrint(int N, int fin, boolean toggle) {
+        if (!toggle && N == fin) {
+            System.out.print(N);
+            return;
+        }
+
+        if (N == 1) {
+            System.out.print("1" + " ");
+            toggle = false;
+        }
+
+        if (toggle) {
+            System.out.print(N + " ");
+            recurvePrint(N - 1, fin, true);
+        } else {
+            System.out.print(N + " ");
+            recurvePrint(N + 1, fin, false);
+        }
+    }
+
+    public static Scanner sc = new Scanner(System.in);
+    public static int scanInt() {
+        return sc.nextInt();
+    }
+}
